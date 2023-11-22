73af8d875a719b9de0fde588d1aebfadd44af040
[mohmmedlahlali20] <[mohmmedlaeh81@gmail.com]>
2023-11-22 22:36:14 +0100

    frontend-electroNacer2-breif6

---
+++ product.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./Layout/css/stayl.css">
+ 
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+  <title>Catalogue de Produits</title>
+</head>
+<body class="bg-gray-100 p-8">
+
+  <div class="container mx-auto dii">
+    <h1 style="font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+        font-size: 50px;
+        color: #2504ff;
+        font-weight: bold;
+        margin-top: 10px;
+        margin-bottom: 10px;
+        margin-left: 10px;
+        margin-right: 10px;
+        text-align:center;
+        " id="titels" class="text-3xl font-semibold mb-8">Catalogue de Produits</h1>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+      <!-- Produit 1 -->
+      <div class="bg-white p-4 rounded-lg shadow-md">
+        <img src="./img/87c33c6e8560da43dd58090235cebf02.jpg" alt="Produit 1" class="mb-4 rounded">
+        <h2 class="text-lg font-semibold mb-2">Produit 1</h2>
+        <p class="text-gray-600">Description du produit 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p class="text-blue-500 mt-2">$19.99</p>
+        <button class="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">buy now</button>
+      </div>
+
+      <!-- Produit 2 -->
+      <div class="bg-white p-4 rounded-lg shadow-md">
+        <img src="./img/87c33c6e8560da43dd58090235cebf02.jpg" alt="Produit 2" class="mb-4 rounded">
+        <h2 class="text-lg font-semibold mb-2">Produit 2</h2>
+        <p class="text-gray-600">Description du produit 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p class="text-blue-500 mt-2">$29.99</p>
+        <button class="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">buy now</button>
+      </div>
+      <!-- produit 3 -->
+      <div class="bg-white p-4 rounded-lg shadow-md">
+        <img src="./img/87c33c6e8560da43dd58090235cebf02.jpg" alt="Produit 2" class="mb-4 rounded">
+        <h2 class="text-lg font-semibold mb-2">Produit 2</h2>
+        <p class="text-gray-600">Description du produit 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p class="text-blue-500 mt-2">$29.99</p>
+        <button class="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">buy now</button>
+      </div>
+      <!-- produit 4 -->
+      <div class="bg-white p-4 rounded-lg shadow-md">
+        <img src="./img/87c33c6e8560da43dd58090235cebf02.jpg" alt="Produit 2" class="mb-4 rounded">
+        <h2 class="text-lg font-semibold mb-2">Produit 2</h2>
+        <p class="text-gray-600">Description du produit 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p class="text-blue-500 mt-2">$29.99</p>
+        <button class="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">buy now</button>
+      </div>
+      <div class="bg-white p-4 rounded-lg shadow-md">
+        <img src="./img/87c33c6e8560da43dd58090235cebf02.jpg" alt="Produit 1" class="mb-4 rounded">
+        <h2 class="text-lg font-semibold mb-2">Produit 1</h2>
+        <p class="text-gray-600">Description du produit 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p class="text-blue-500 mt-2">$19.99</p>
+        <button class="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">buy now</button>
+      </div>
+
+      <!-- Produit 2 -->
+      <div class="bg-white p-4 rounded-lg shadow-md">
+        <img src="./img/87c33c6e8560da43dd58090235cebf02.jpg" alt="Produit 2" class="mb-4 rounded">
+        <h2 class="text-lg font-semibold mb-2">Produit 2</h2>
+        <p class="text-gray-600">Description du produit 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p class="text-blue-500 mt-2">$29.99</p>
+        <button class="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">buy now</button>
+      </div>
+      <!-- produit 3 -->
+      <div class="bg-white p-4 rounded-lg shadow-md">
+        <img src="./img/87c33c6e8560da43dd58090235cebf02.jpg" alt="Produit 2" class="mb-4 rounded">
+        <h2 class="text-lg font-semibold mb-2">Produit 2</h2>
+        <p class="text-gray-600">Description du produit 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p class="text-blue-500 mt-2">$29.99</p>
+        <button class="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">buy now</button>
+      </div>
+      <!-- produit 4 -->
+      <div class="bg-white p-4 rounded-lg shadow-md">
+        <img src="./img/87c33c6e8560da43dd58090235cebf02.jpg" alt="Produit 2" class="mb-4 rounded">
+        <h2 class="text-lg font-semibold mb-2">Produit 2</h2>
+        <p class="text-gray-600">Description du produit 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p class="text-blue-500 mt-2">$29.99</p>
+        <button class="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">buy now</button>
+      </div> 
+      <div class="bg-white p-4 rounded-lg shadow-md">
+        <img src="./img/87c33c6e8560da43dd58090235cebf02.jpg" alt="Produit 1" class="mb-4 rounded">
+        <h2 class="text-lg font-semibold mb-2">Produit 1</h2>
+        <p class="text-gray-600">Description du produit 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p class="text-blue-500 mt-2">$19.99</p>
+        <button class="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">buy now</button>
+      </div>
+
+      <!-- Produit 2 -->
+      <div class="bg-white p-4 rounded-lg shadow-md">
+        <img src="./img/87c33c6e8560da43dd58090235cebf02.jpg" alt="Produit 2" class="mb-4 rounded">
+        <h2 class="text-lg font-semibold mb-2">Produit 2</h2>
+        <p class="text-gray-600">Description du produit 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p class="text-blue-500 mt-2">$29.99</p>
+        <button class="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">buy now</button>
+      </div>
+      <!-- produit 3 -->
+      <div class="bg-white p-4 rounded-lg shadow-md">
+        <img src="./img/87c33c6e8560da43dd58090235cebf02.jpg" alt="Produit 2" class="mb-4 rounded">
+        <h2 class="text-lg font-semibold mb-2">Produit 2</h2>
+        <p class="text-gray-600">Description du produit 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p class="text-blue-500 mt-2">$29.99</p>
+        <button class="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">buy now</button>
+      </div>
+      <!-- produit 4 -->
+      <div class="bg-white p-4 rounded-lg shadow-md">
+        <img src="./img/87c33c6e8560da43dd58090235cebf02.jpg" alt="Produit 2" class="mb-4 rounded">
+        <h2 class="text-lg font-semibold mb-2">Produit 2</h2>
+        <p class="text-gray-600">Description du produit 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <p class="text-blue-500 mt-2">$29.99</p>
+        <button class="mt-4 bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700">buy nowr</button>
+      </div>
+
+
+      <!-- Ajoutez plus de produits ici -->
+
+    </div>
+  </div>
+
+  
+<script src="./Layout/js/script.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.js" defer></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/zxcvbn/4.4.2/zxcvbn.js"></script>
+</body>
+</html>
